de140039302007d884f60c8c2af4ddab83cadfee
dlfjs0472 <dlfjs0472@gmail.com>
2021-07-28 08:10:11 +0900

    쇼핑몰 AOP를 이용한 로그인 처리

--- ShoppingMall/src/main/java/com/koreait/shoppingmall/model/repository/admin/MybatisAdminDAO.java
+++ ShoppingMall/src/main/java/com/koreait/shoppingmall/model/repository/admin/MybatisAdminDAO.java
@@ -11,42 +11,41 @@ import com.koreait.shoppingmall.exception.MemberExistException;
 
 @Repository
 public class MybatisAdminDAO implements AdminDAO{
-   
-   @Autowired
-   private SqlSessionTemplate sqlSessionTemplate;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	@Override
+	public Admin login(Admin admin) throws MemberExistException {
+		Admin obj=sqlSessionTemplate.selectOne("Admin.login", admin);
+		if(obj==null) {
+			throw new MemberExistException("관리자 정보가 올바르지 않습니다");
+		}
+		return obj;
+	}
 
-   @Override
-   public Admin login(Admin admin) throws MemberExistException{
-      Admin obj=sqlSessionTemplate.selectOne("Admin.login", admin);
-      if(obj==null) {
-    	  throw new MemberExistException("\n 관리자 정보가 올바르지 않습니다");
-      }
-      
-      return obj;
-   }
+	@Override
+	public void regist(Admin admin) {
+		// TODO Auto-generated method stub
+		
+	}
 
-   @Override
-   public void regist(Admin admin) {
-      // TODO Auto-generated method stub
-      
-   }
+	@Override
+	public void update(Admin admin) {
+		// TODO Auto-generated method stub
+		
+	}
 
-   @Override
-   public Admin update(Admin admin) {
-      // TODO Auto-generated method stub
-      return null;
-   }
+	@Override
+	public void delete(Admin admin) {
+		// TODO Auto-generated method stub
+		
+	}
 
-   @Override
-   public void delete(Admin admin) {
-      // TODO Auto-generated method stub
-      
-   }
-
-   @Override
-   public List selectAll() {
-      // TODO Auto-generated method stub
-      return null;
-   }
-
+	@Override
+	public List selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
